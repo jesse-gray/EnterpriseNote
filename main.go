df8b789529ed86c29286e549ce32a572040d60c9
Jesse Gray <jessegray13@hotmail.com>
2019-10-03 20:19:06 +1300

    Added the APIs for users - same routing as the Notes

--- main.go
+++ main.go
@@ -24,8 +24,9 @@ type User struct {
 	LastName  string `json:"lastname"`
 }
 
-//Initialise notes slice
+//Initialise slices
 var notes []Note
+var users []User
 
 //Get ALL notes
 func getNotes(w http.ResponseWriter, r *http.Request) {
@@ -88,6 +89,67 @@ func updateNote(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(notes)
 }
 
+//Get ALL users
+func getUsers(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(users)
+}
+
+//Get single user
+func getUser(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	//Loop through users for the right id
+	for _, user := range users {
+		if user.UserID == params["id"] {
+			json.NewEncoder(w).Encode(user)
+			return
+		}
+	}
+	json.NewEncoder(w).Encode(&User{})
+}
+
+//Create a new user
+func createUser(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	var user User
+	_ = json.NewDecoder(r.Body).Decode(&user)
+	user.UserID = strconv.Itoa(rand.Intn(1000000)) //Mock ID - not safe
+	users = append(users, user)
+	json.NewEncoder(w).Encode(user)
+}
+
+//Delete a user
+func deleteUser(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	for index, user := range users {
+		if user.UserID == params["id"] {
+			users = append(users[:index], users[index+1:]...)
+			break
+		}
+	}
+	json.NewEncoder(w).Encode(users)
+}
+
+//Update a user
+func updateUser(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	for index, user := range users {
+		if user.UserID == params["id"] {
+			users = append(users[:index], users[index+1:]...)
+			var user User
+			_ = json.NewDecoder(r.Body).Decode(&user)
+			user.UserID = params["id"]
+			users = append(users, user)
+			json.NewEncoder(w).Encode(user)
+			return
+		}
+	}
+	json.NewEncoder(w).Encode(users)
+}
+
 func main() {
 	//Initialise Router
 	r := mux.NewRouter()
@@ -96,12 +158,20 @@ func main() {
 	notes = append(notes, Note{NoteID: "1", NoteText: "This is sample text for the first note", Author: &User{UserID: "1", FirstName: "John", LastName: "Smith"}})
 	notes = append(notes, Note{NoteID: "2", NoteText: "This is some more sample text, however this is for the second note", Author: &User{UserID: "2", FirstName: "Sharon", LastName: "Tomkins"}})
 
+	users = append(users, User{UserID: "1", FirstName: "John", LastName: "Smith"})
+	users = append(users, User{UserID: "2", FirstName: "Sharon", LastName: "Tomkins"})
+
 	//Route handlers
 	r.HandleFunc("/api/notes", getNotes).Methods("GET")
 	r.HandleFunc("/api/notes/{id}", getNote).Methods("GET")
 	r.HandleFunc("/api/notes", createNote).Methods("POST")
 	r.HandleFunc("/api/notes/{id}", updateNote).Methods("PUT")
 	r.HandleFunc("/api/notes/{id}", deleteNote).Methods("DELETE")
+	r.HandleFunc("/api/users", getUsers).Methods("GET")
+	r.HandleFunc("/api/users/{id}", getUser).Methods("GET")
+	r.HandleFunc("/api/users", createUser).Methods("POST")
+	r.HandleFunc("/api/users/{id}", updateUser).Methods("PUT")
+	r.HandleFunc("/api/users/{id}", deleteUser).Methods("DELETE")
 	log.Fatal(http.ListenAndServe(":8000", r))
 
 }
