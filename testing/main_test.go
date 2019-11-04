99a99ddb7a16fd97538217c0cd9717fa32827d72
sjdolley <garfunkalman2@gmail.com>
2019-11-05 12:16:40 +1300

    meh, deleted

---
+++ testing/main_test.go
@@ -0,0 +1,12 @@
+package testing
+
+import (
+	"testing"
+	"net/http"
+	"net/http/httptest"
+	"database/sql"
+	"errors"
+
+	"github.com/stretchr/testify/assert"
+)
+
---
+++ testing/notes_test.go
@@ -0,0 +1,19 @@
+package testing
+
+import (
+	"testing"
+	"net/http"
+	"net/http/httptest"
+	"database/sql"
+	"errors"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestCreateNote(t *Testing.T) {
+
+}
+
+func TestGetNote(t *Testing.T) {
+	
+}
