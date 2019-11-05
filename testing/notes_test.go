06688e453d795c16e2bcfb6c2cba843c906d1669
sjdolley <garfunkalman2@gmail.com>
2019-11-05 13:50:16 +1300

    first unit test written in notes_test.go

--- testing/notes_test.go
+++ testing/notes_test.go
@@ -15,5 +15,21 @@ func TestCreateNote(t *Testing.T) {
 }
 
 func TestGetNote(t *Testing.T) {
+	req, err := http.NewRequest("GET", "/api/note/{id}/{user}", nil)
+	if err != nil{
+		t.Fatal(err)
+	}
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(getNote)
+	handler.ServeHTTP(rr, req)
+	if status := rr.Code; status != http.StatusOK {
+		t.Errorf("handler returned wrong status code: got %v want %v", status, http.StatusOK)
+	}
 	
+	//check the response body is what we expect.
+
+expected := `[{"note_id":1, "note_text":"This is sample text for the first note", "author_id":1}`
+if rr.Body.String() != expected {
+	t.Errorf("handler returned unexpected data got %v want %v", rr.Body.String(), expected)
+}
 }
