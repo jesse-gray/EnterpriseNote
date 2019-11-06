f06bb43d76ec89186388834dbdec07ed846ab627
sjdolley <garfunkalman2@gmail.com>
2019-11-06 18:57:37 +1300

    added test for create note  - still need to figure out stubbing

--- notes_test.go
+++ notes_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bytes"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -27,5 +28,26 @@ func TestGetNote(t *testing.T) {
 	expected := `[{"note_id":1, "note_text":"This is sample text for the first note", "author_id":1}`
 	if rr.Body.String() != expected {
 		t.Errorf("handler returned unexpected data got %v want %v", rr.Body.String(), expected)
+	}
+}
+
+func TestCreateNote(t *testing) {
+
+	var jsonStr = []byte(`{"note_text":"Hello World", "author_id":1}`)
+
+	req, err := http.NewRequest("Post", "/api/notes/{bool}", bytes.NewBuffer(jsonStr))
+	if err != nil {
+		t.Fatal(err)
+	}
+	req.Header.Set("Content-Type", "application/json")
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(createNote)
+	handler.ServeHTTP(rr, req)
+	if status := rr.Code; status != http.StatusOk {
+		t.Errorf("handler returned wrong status code: got %v want %v", status, http.StatusOK)
+	}
+	expected := `{"note_text":"Hello World", "author_id":1}`
+	if rr.Body.String() != expected {
+		t.Errorf("handler returned unexpected body: got %v want %v", rr.Body.String(), expected)
 	}
 }
