a39c247514665fd8e1259afb8d05315e617df204
sjdolley <garfunkalman2@gmail.com>
2019-11-06 20:14:49 +1300

    added users_test

--- users_test.go
+++ users_test.go
@@ -7,29 +7,9 @@ import (
 	"testing"
 )
 
-func TestGetNote(t *testing.T) {
-	req, err := http.NewRequest("GET", "/api/note/{id}/{user}", nil)
-	if err != nil {
-		t.Fatal(err)
-	}
-	rr := httptest.NewRecorder()
-	handler := http.HandlerFunc(getNote)
-	handler.ServeHTTP(rr, req)
-	if status := rr.Code; status != http.StatusOK {
-		t.Errorf("handler returned wrong status code: got %v want %v", status, http.StatusOK)
-	}
+func TestCreateUser(t *testing.T) {
 
-	//check the response body is what we expect.
-
-	expected := `[{"note_id":1, "note_text":"This is sample text for the first note", "author_id":1}`
-	if rr.Body.String() != expected {
-		t.Errorf("handler returned unexpected data got %v want %v", rr.Body.String(), expected)
-	}
-}
-
-func TestCreateNote(t *testing.T) {
-
-	var jsonStr = []byte(`{"note_text":"Hello World", "author_id":1}`)
+	var jsonStr = []byte(`{"user_first_name":"Harlod", "user_last_name":"Boomkin", "cookie_id":"", "user_password":"password"}`)
 
 	req, err := http.NewRequest("Post", "/api/notes/{bool}", bytes.NewBuffer(jsonStr))
 	if err != nil {
@@ -37,18 +17,18 @@ func TestCreateNote(t *testing.T) {
 	}
 	req.Header.Set("Content-Type", "application/json")
 	rr := httptest.NewRecorder()
-	handler := http.HandlerFunc(createNote)
+	handler := http.HandlerFunc(createUser)
 	handler.ServeHTTP(rr, req)
 	if status := rr.Code; status != http.StatusOK {
 		t.Errorf("handler returned wrong status code: got %v want %v", status, http.StatusOK)
 	}
-	expected := `{"note_text":"Hello World", "author_id":1}`
+	expected := `{"user_first_name":"Harlod", "user_last_name":"Boomkin", "cookie_id":"", "user_password":"password"}`
 	if rr.Body.String() != expected {
 		t.Errorf("handler returned unexpected body: got %v want %v", rr.Body.String(), expected)
 	}
 }
 
-func TestDeleteNote(t *testing.T) {
+func TestDeleteUser(t *testing.T) {
 
 	req, err := http.NewRequest("DELETE", "/entry", nil)
 	if err != nil {
@@ -56,7 +36,7 @@ func TestDeleteNote(t *testing.T) {
 	}
 
 	q := req.URL.Query()
-	q.Add("note_id", "1")
+	q.Add("user_id", "1")
 	req.URL.RawQuery = q.Encode()
 	rr := httptest.NewRecorder()
 	handler := http.HandlerFunc(deleteNote)
@@ -64,7 +44,7 @@ func TestDeleteNote(t *testing.T) {
 	if status := rr.Code; status != http.StatusOK {
 		t.Errorf("handler returned wrong status code: got %v want %v", status, http.StatusOK)
 	}
-	expected := `{"note_id":1,"note_text":"This is a sample text for the first note","author_id":1}`
+	expected := `{"user_first_name":"John", "user_last_name":"Acers", "cookie_id":"", "user_password":"password"}`
 	if rr.Body.String() != expected {
 		t.Errorf("handler returned unexpected body: got %v want %v", rr.Body.String(), expected)
 	}
@@ -72,7 +52,7 @@ func TestDeleteNote(t *testing.T) {
 
 func TestUpdateNote(t *testing.T) {
 
-	var jsonStr = []byte(`{"note_id":1, "note_text":"Hello World", "author_id":1}`)
+	var jsonStr = []byte(`{"user_id":1, "user_first_name":"Gerald", "user_last_name":"Hopkins", "cookie_id":"", "user_password":"password"}`)
 
 	req, err := http.NewRequest("PUT", "/api/notes/{id}", bytes.NewBuffer(jsonStr))
 	if err != nil {
@@ -86,10 +66,10 @@ func TestUpdateNote(t *testing.T) {
 		t.Errorf("handler returned wrong status code: got %v want %v",
 			status, http.StatusOK)
 	}
-	expected := `{"note_id":1, "note_text":"Hello World", "author_id":1}`
+	expected := `{"user_id":1, "user_first_name":"Gerald", "user_last_name":"Hopkins", "cookie_id":"", "user_password":"password"}`
 	if rr.Body.String() != expected {
 		t.Errorf("handler returned unexpected body: got %v want %v", rr.Body.String(), expected)
 	}
 }
 
-// func TestGetNotes -- Need to find out how to test recieving an array of objects
+// need to add test for getUsers
