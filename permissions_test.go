e5add712490f40bd5133192c64039278f624c173
sjdolley <garfunkalman2@gmail.com>
2019-11-06 21:04:02 +1300

    permissions_test partially complete

--- permissions_test.go
+++ permissions_test.go
@@ -49,3 +49,26 @@ func TestCreateFavourite(t *testing.T) {
 		t.Errorf("handler returned unexpected body: got %v want %v", rr.Body.String(), expected)
 	}
 }
+
+func TestDeleteFavourite(t *testing.T) {
+
+	req, err := http.NewRequest("DELETE", "/api/favourite/{id}", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	q := req.URL.Query()
+	q.Add("user_id", "1")
+	q.Add("note_id", "1")
+	req.URL.RawQuery = q.Encode()
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(deleteUser)
+	handler.ServeHTTP(rr, req)
+	if status := rr.Code; status != http.StatusOK {
+		t.Errorf("handler returned wrong status code: got %v want %v", status, http.StatusOK)
+	}
+	expected := `{"user_first_name":"John", "user_last_name":"Acers", "cookie_id":"", "user_password":"password"}`
+	if rr.Body.String() != expected {
+		t.Errorf("handler returned unexpected body: got %v want %v", rr.Body.String(), expected)
+	}
+}
